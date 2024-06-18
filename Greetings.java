97b5b2ec494299f3cc08c5f0ef9beaaae6a9b1c0
mukundaDev <94097024+mukundaDev@users.noreply.github.com>
2024-06-18 15:46:35 +0530

    Added a line

--- Greetings.java
+++ Greetings.java
@@ -4,5 +4,6 @@ public class Greetings {
     public static void main(String[] args) {
         System.out.println("Good Afternoon");
         System.out.println("Hello, Greetings");
+        System.out.println("********");
     }
 }
